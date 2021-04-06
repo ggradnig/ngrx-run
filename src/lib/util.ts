d49878df372b8b69cc96b5e1789b1e03ffe49a0e
Gerhard <contact.gradnig@gmail.com>
2021-04-06 22:07:18 +0200

    Add status utility type

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,1 +1,55 @@
 export type ActionsOf<T extends { [key: string]: (...args: any) => any }> = ReturnType<T[keyof T]>;
+
+export enum StatusTypes {
+  loading = 'loading',
+  loaded = 'loaded',
+  failed = 'failed'
+}
+
+export interface Either<T> {
+  orElse<U>(other: U): T | U;
+}
+
+export class Loading<T> implements Either<T> {
+  type = StatusTypes.loading;
+
+  orElse<U>(other: U): U {
+    // @ts-ignore
+    return undefined;
+  }
+
+  map<U>(f: (a: T) => U): Status<U> {
+    return (this as unknown) as Status<U>;
+  }
+}
+
+export class Loaded<T> implements Either<T> {
+  type = StatusTypes.loaded;
+
+  constructor(public value: T) {}
+
+  orElse<U>(other: U): T | U {
+    return this.value ?? other;
+  }
+
+  map<U>(f: (a: T) => U): Status<U> {
+    return new Loaded(f(this.value)) as Status<U>;
+  }
+}
+
+export class Failed<T> implements Either<T> {
+  type = StatusTypes.failed;
+
+  constructor(public error: string) {}
+
+  orElse<U>(other: U): U {
+    // @ts-ignore
+    return undefined;
+  }
+
+  map<U>(f: (a: T) => U): Status<U> {
+    return (this as unknown) as Status<U>;
+  }
+}
+
+export type Status<T> = Loading<T> | Loaded<T> | Failed<T>;
