193b85531f2f3f69dc7252a8f18803864eb91330
Gerhard <contact.gradnig@gmail.com>
2021-10-09 21:40:57 +0200

    Add tokenization of actions

--- modules/lib/public_api.ts
+++ modules/lib/public_api.ts
@@ -6,6 +6,6 @@ export {unsubscribe} from './src/unsubscribe';
 export {run} from './src/run';
 export {ReducerResult as StateWithEffects, ActionReducer} from './src/reducer';
 export {EffectStoreModule} from './src/module';
-export {ActionsOf, isActionOf, childReducer} from './src/extra-utils';
+export {ActionsOf, isActionOf, childReducer, tokenized, hasToken, createToken} from './src/extra-utils';
 export {EffectConfig} from './src/effect-config';
 export {SubscriptionToken} from './src/effect';
--- modules/lib/src/extra-utils.ts
+++ modules/lib/src/extra-utils.ts
@@ -1,21 +1,20 @@
-import { ActionCreator } from '@ngrx/store';
-import { TypedAction } from '@ngrx/store/src/models';
+import { Action, ActionCreator } from '@ngrx/store';
 import { ReducerResult } from './reducer';
 
 export type ActionsOf<T extends { [key: string]: (...args: any) => any }> = ReturnType<
   T[keyof T]
 >;
 
-export function isActionOf<T extends { [key: string]: ActionCreator }, K extends string>(
-  action: TypedAction<K>,
+export function isActionOf<T extends { [key: string]: ActionCreator }>(
+  action: Action,
   actions: T
-): action is ActionsOf<T> & TypedAction<K> {
+): action is ActionsOf<T> & Action {
   return Object.values(actions)
     .map((a) => a.type)
     .includes(action.type);
 }
 
-export function childReducer<S, K extends keyof S, A extends TypedAction<string>>(
+export function childReducer<S, K extends keyof S, A extends Action>(
   state: S,
   key: K,
   action: A,
@@ -28,3 +27,31 @@ export function childReducer<S, K extends keyof S, A extends TypedAction<string>
 }
 
 type InitializedActionReducer<T, A> = (state: T, action: A) => ReducerResult<T>;
+
+export function tokenized<T, K extends string, P extends object>(
+  actionCreator: ActionCreator<K, (props: P) => P & Action>,
+  token: Token<P>
+): ActionCreator<K, (props: P) => P & TypedAction<K>> & Tokenized<T, P, K> {
+  return Object.assign(actionCreator, { token }) as any;
+}
+
+export function hasToken<P>(maybeTokenized: Action, token: Token<P>): maybeTokenized is Action & P {
+  return isTokenized(maybeTokenized) && maybeTokenized.token === token;
+}
+
+export function createToken<U>(token: string): Token<U> {
+  return token as Token<U>;
+}
+
+type Token<U> = string & U;
+
+function isTokenized(maybeTokenized: any): maybeTokenized is Tokenized<any, any, any> {
+  return maybeTokenized.token != null;
+}
+
+type Tokenized<T, P, K extends string> = { token: T } & PropsFn<P, K>;
+type PropsFn<P, K extends string> = (props: P) => P & TypedAction<K>;
+
+interface TypedAction<T extends string> extends Action {
+  readonly type: T;
+}
